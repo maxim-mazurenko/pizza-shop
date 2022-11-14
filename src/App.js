444e6df1d8efad84e6e71883b318bf2b4905443e
Maksim Mazurenko <77229020+maxim-mazurenko@users.noreply.github.com>
2022-11-14 19:50:15 +0300

    start of development app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Привет мир!</h1>
+    </div>
+  );
+}
+
+function Asd() {
+  return <h2>sdf</h2>;
+}
+
+export default App;
+export { Asd };
